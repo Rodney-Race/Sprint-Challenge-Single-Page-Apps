ac003ce4f88ca3aae7b548f455a7eb37e9dbe936
Rodney Race <rodneyrace@gmail.com>
2019-12-13 21:48:25 -0500

    update

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const characterData = props.characterData;
+  return (
+    <>
+      {characterData.map(character => {
+        return (
+          <WrapperDiv className="characterWrapper">
+            <img src={character.image} />
+            <h1>{character.name}</h1>
+            <h2>{character.status}</h2>
+            <h3>{character.species}</h3>
+          </WrapperDiv>
+        );
+      })}
+    </>
+  ); 
 }
