ce8cd06ec04560cc11883386a835b6e246ddf480
Rodney Race <rodneyrace@gmail.com>
2019-12-13 22:55:56 -0500

    update

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard(props) {
+  const locationData = props.locationData;
+  return (
+    <>
+      {locationData.map(location => {
+        return (
+          <WrapperDiv className="locationWrapper">
+            <h1>{location.name}</h1>
+            <h2>{location.type}</h2>
+            <h3>{location.dimension}</h3>
+          </WrapperDiv>
+        );
+      })}
+    </>
+  ); 
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -11,13 +11,14 @@ export default function SearchForm({ onSearch }) {
   return (
     <section className="search-form">
      <form onSubmit = {() => onSearch(query)}>
-       <inputonChange = {handleInputChange}
-       placeholder = "name"
-       value = {query.name}
-       name = "name"
+       <input
+         onChange = {handleInputChange}
+         placeholder = "name"
+         value = {query.name}
+         name = "name"
        />
        <button type = "submit">Search</button>
-     </form>
+      </form>
     </section>
   );
 }
